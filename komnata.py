f96376664a21be2e3106041a0480c02fbad096d9
MichalJ <rekwizytornia@hotmail.com>
2024-05-06 14:29:16 +0200

    Dodanie automatycznego tworzenia przesc do kolejnych komnat w osi wschód-zachód

--- komnata.py
+++ komnata.py
@@ -23,8 +23,8 @@ def create_description():
 
 
 def create_room():
-    number_of_exits = random.randint(1,3)
-    print(number_of_exits)
+    # number_of_exits = random.randint(1,3)
+    # print(f"number_of_exits: {number_of_exits}")
     komnata = Komnata(f"Komnata {len(komnaty)}", f"{create_description()}.")
     komnaty.append(komnata)
     # name = f"room_{len(komnaty)+1}" #ustawienie poczatkowej nazwy instancji
@@ -34,32 +34,14 @@ def create_room():
 def change_room(room):
     #previous_room = current_room
     current_room = komnaty[room]
-    print(f"Wchodzisz do [{current_room.name}]. {current_room.description}")
-    print(f"  Wyjścia: {current_room.exits}")
+    print(f"\nWchodzisz do [{current_room.name}]. \n  {current_room.description}")
+    #print(f"  Wyjścia: {current_room.exits}")
+    print("   Możliwe wyjścia:")
+    for key, value in current_room.exits.items():
+        if value != None:
+            print(f"   - {key}: {value}")
     return current_room
 
-komnata = Komnata(f"Komnata 0", f"Komnata początkowa")
-komnaty.append(komnata)
-print(f"Tworzę komnatę o nazwie: {komnata.name}")
-
-current_room = komnata
-
-for n in range(3):
-    create_room()
-
-komnaty[0].exits["północ"] = komnaty[1].name #zmiana wyjścia z komnaty początkowej z "None" na komnata 1
-komnaty[1].exits["południe"] = komnaty[0].name #zmiana wyjścia z komnaty 1 z "None" na komnata początkową
-
-i = 0
-for n in range(len(komnaty)):
-    print(f"{komnaty[i].name}: {komnaty[i].description}")
-    print(f"  Wyścia: {komnaty[i].exits}\n")
-    i += 1
-
-# i = 0
-# for n in range(len(komnaty)):
-#     change_room(i)
-#     i += 1
 
 def players_move():
     a = int(input(f"Dokąd chcesz iść? "))
@@ -69,13 +51,39 @@ def players_move():
     current_room = change_room(a)
     return current_room
 
+#main##############################################
 
-print(f"Znajdujesz się w [{current_room.name}]. {current_room.description}")
-print(f"  Wyjścia: {current_room.exits}")
-while True:
-    current_room = players_move()
-    #print(f"{current_room.name}: {current_room.description}") #test na to czy current room to ta sama do ktorej weszlismy
+#Stworzenie pierwszej komnaty
+komnata = Komnata(f"Komnata 0", f"Komnata początkowa")
+komnaty.append(komnata)
+print(f"Tworzę komnatę o nazwie: {komnata.name}")
+
+current_room = komnata #ustawienie początkowej lokacji gracza
+
+#Stworzenie pozostałych komnat i ścieżek
+z = 0
+for n in range(5):
+    create_room()
+    komnaty[z].exits["wschód"] = komnaty[z+1].name 
+    komnaty[z+1].exits["zachód"] = komnaty[z].name
+    z += 1
+
+print("\n---Lista stworzonych komnat i wyjść:")
+i = 0
+for n in range(len(komnaty)):
+    print(f"{komnaty[i].name}: {komnaty[i].description}")
+    print(f"  Wyścia: {komnaty[i].exits}")
+    i += 1
+
+print("\n---Test chodzenia po komnatach")
+i = 0
+for n in range(len(komnaty)):
+    change_room(i)
+    i += 1
 
 
-
-
+# print(f"Znajdujesz się w [{current_room.name}]. {current_room.description}")
+# print(f"  Wyjścia: {current_room.exits}")
+# while True:
+#     current_room = players_move()
+#     #print(f"{current_room.name}: {current_room.description}") #test na to czy current room to ta sama do ktorej weszlismy
